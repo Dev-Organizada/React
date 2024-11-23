1087781b562588cdca46fb95696748311462c648
Dev-Organizada <devorganizada@gmail.com>
2024-11-23 10:46:39 -0300

    Anotações sobre o curso

---
+++ Anotacoes .js
@@ -0,0 +1,16 @@
+-- React é uma biblioteca JavaScript para desenvolvimento de aplicações front-end
+-- O Node.js é uma runtime de JavaScript, ou seja, uma biblioteca utilizada por um compilador
+-- npm é um gerenciador de pacotes do Node.
+-- npx é um executor de scripts
+ 
+//Comandos
+Criação de uma nova aplicação -- npm create vite@latest
+Iniciar a aplicação -- npm run dev
+ 
+-- Repositorio do curso: https://github.com/matheusbattisti/curso_react
+
+//Hooks
+-- Recursos do react q tem diversas funções, como guardar e alterar o estado de algum dado, por exemplo.
+-- Todos os Hooks começam com use, por exemplo: useState
+-- Pode-se criar um Hooks, isso é chamado de custom Hook
+-- Precisam ser importados
