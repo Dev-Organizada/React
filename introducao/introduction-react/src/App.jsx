7ba662c5a71bb9fc478d008216fe0acb1fe284d2
Dev-Organizada <devorganizada@gmail.com>
2024-11-23 10:32:14 -0300

    Trabalhando com imagens

--- introducao/introduction-react/src/App.jsx
+++ introducao/introduction-react/src/App.jsx
@@ -3,6 +3,9 @@ import FirstComponent from './components/FirstComponent'
 import Event from './components/Event'
 import Challenge from './components/Challenge'
 
+// image
+import ImageAda from './assets/ada.jpg'
+
 // styles
 import './App.css'
 
@@ -11,10 +14,16 @@ import './App.css'
 function App() {
   return (
       <>
-        <h1>Hello World</h1>
-        <FirstComponent />
-        <Event/>
-        <Challenge/>
+        <h1>Avançando em React</h1>
+        <p>Adicionando imagem via public</p>
+        <div>
+          <img src="code.jpg" alt="simbolo-react" />
+        </div>
+
+        <p>Adicionando imagem via assets</p>
+        <div>
+          <img src={ImageAda} alt="Ada Programadora" />
+        </div>
       </>
   )
 }
