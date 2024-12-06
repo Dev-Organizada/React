8076c8bb9a31cdd0f38e712ed21cdfbdd8969fd8
Dev-Organizada <devorganizada@gmail.com>
2024-12-05 21:21:19 -0300

    Passando valor para o input

--- introducao/Form-React/Forms_React/src/App.jsx
+++ introducao/Form-React/Forms_React/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h2>Forms</h2>
-      <MyForm/>
+      <MyForm user={{name:"Fernanda", email:"fernanda@teste.com"}}/>
     </>
   )
 }
--- introducao/Form-React/Forms_React/src/components/MyForm.jsx
+++ introducao/Form-React/Forms_React/src/components/MyForm.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import './MyForm.css'
 
-const MyForm = () => {
+const MyForm = ({user}) => {
     {/* Gerenciamento de dados */}
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
+    const [name, setName] = useState(user ? user.name : "") // Recebendo o valor da prop
+    const [email, setEmail] = useState(user ? user.email : "")   
 
     {/* Resgatar o valor digitado no input */}
     const handleName = (e) => {
@@ -29,6 +29,7 @@ const MyForm = () => {
                     name='name' 
                     placeholder='Digite o seu Nome' 
                     onChange={handleName}
+                    value={name} 
                 />
             </div>
 
@@ -40,6 +41,7 @@ const MyForm = () => {
                     name='email' 
                     placeholder='Digite o seu E-mail' 
                     onChange={(e) => setEmail(e.target.value)}
+                    value={email}
                 />
             </label>
 
