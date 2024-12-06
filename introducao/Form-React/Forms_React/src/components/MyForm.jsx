0b86dd676f71aff3430ecc26e8aac0d9a6048d39
Dev-Organizada <devorganizada@gmail.com>
2024-12-05 21:13:53 -0300

    Envio de formulario

--- introducao/Form-React/Forms_React/src/components/MyForm.jsx
+++ introducao/Form-React/Forms_React/src/components/MyForm.jsx
@@ -1,19 +1,46 @@
+import { useState } from 'react'
 import './MyForm.css'
 
 const MyForm = () => {
+    {/* Gerenciamento de dados */}
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    {/* Resgatar o valor digitado no input */}
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    {/* Função para enviar o formulario */}
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Eviando o formulario")
+        console.log(name, email)
+    }
+
   return (
     <div>
         {/* Criação de Formulario */}
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor='name'>Nome: </label>
-                <input type='text' name='name' placeholder='Digite o seu Nome' />
+                <input 
+                    type='text' 
+                    name='name' 
+                    placeholder='Digite o seu Nome' 
+                    onChange={handleName}
+                />
             </div>
 
             {/* Label envolvendo o input */}
             <label>
                 <span>E-mail</span>
-                <input type='email' name='email' placeholder='Digite o seu E-mail' />
+                <input 
+                    type='email' 
+                    name='email' 
+                    placeholder='Digite o seu E-mail' 
+                    onChange={(e) => setEmail(e.target.value)}
+                />
             </label>
 
             <input type='submit' value='Enviar' />
